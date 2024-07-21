ede6524cb130b9d48de207531730811202f25bf9
Rakesh Vuyyuru <146849094+rakey09@users.noreply.github.com>
2024-07-21 17:10:36 +0530

    Matrix Addition

---
+++ Matrix Addition.java
@@ -0,0 +1,19 @@
+public class MatrixAddition {
+    public static void main(String[] args) {
+        int[][] matrix1 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] matrix2 = {{9, 8, 7}, {6, 5, 4}, {3, 2, 1}};
+        int[][] sum = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                sum[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        System.out.println("Sum of matrices: ");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
